4b023c1fe4192791fa932d59d9e0ace195946b97
sarah-yu <sarahyu0692@gmail.com>
2017-09-26 20:09:59 -0400

    unit 11 assignment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// array of selectable cards
 var cards = [
 	{
 		rank: "queen",
@@ -21,8 +22,10 @@ var cards = [
 	}
 ];
 
+// array of cards selected by user
 var cardsInPlay = [];
 
+// check for match between cards selected
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
@@ -31,16 +34,33 @@ var checkForMatch = function() {
 	};
 };
 
-var flipCard = function(cardId) {
-	if (cardsInPlay.length === 2) {
-		checkForMatch();	
-	};
-	console.log("User flipped " + cards[cardId].rank + ".");
+// flip the selected card and push it into cardsInPlay[]
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+
+	// if two cards have been selected, run checkForMatch()
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+
+	// console messages to check if everything is working 
+	console.log("User flipped " + cards[cardId].rank + ".");
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 };
 
-flipCard(0);
-flipCard(2);
+// create game board with selectable cards
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	};
+};
+
+createBoard();
 
